9c25532648d4263643f1cd56523506c40936d6e7
jconway4 <jconwa4@illinois.edu>
2018-03-17 03:06:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-  
+let main = document.querySelector('body');
 
   
 function init() {
-  let main = document.querySelector('body');
   let i = 0;
   let codeChecker = [];
   
