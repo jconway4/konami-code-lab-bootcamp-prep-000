790a2fe70c5fb5c9b24d19d00e21d49d5c80ab28
jconway4 <jconwa4@illinois.edu>
2018-03-17 03:09:35 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,21 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-  
+var codeFunction = function(e) {
+  const key = parseInt(e.detail || e.which);
+  if (key === code[i]) {
+    i++;
+    codeChecker.push(key);
+      
+    if (codeChecker === code) {
+      alert("Hurray!");
+      i = 0;
+      codeChecker = [];
+    }
+}
+
+
 function init() {
   const body = document.querySelector('body');
   let i = 0;
   let codeChecker = [];
   
   body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.detail || e.which);
-    if (key === code[i]) {
-      i++;
-      codeChecker.push(key);
-      
-      if (codeChecker === code) {
-        alert("Hurray!");
-        i = 0;
-        codeChecker = [];
       }
     }
   });
