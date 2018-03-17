072c0fd478857cff193e96647bf70cd47d99f88e
jconway4 <jconwa4@illinois.edu>
2018-03-17 03:13:19 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 function init() {
   const body = document.querySelector('body');
  
-  main.addEventListener('click', function(event) {
+  body.addEventListener('click', function(event) {
     alert('I was clicked!')
   })
 }
