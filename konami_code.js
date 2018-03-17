5fbc67fb5547ffb80f6dfcd44fc54177cfb470a2
jconway4 <jconwa4@illinois.edu>
2018-03-17 03:15:29 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,23 +1,5 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-let index = 0;
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
  
 function init() {
-  const body = document.querySelector('body');
- 
-  body.addEventListener('keydown', function(event) {
-  const key = parseInt(e.detail || e.which);
- 
-  if (key === code[index]) {
-    index++;
- 
-    if (index === code.length) {
-      alert("Hurray!");
- 
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
-    
-  });
+  // your code here
 }
